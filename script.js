0a87496fb1d6dd2a3814f67c29d027124b89b105
Leong <stapler_1@hotmail.com>
2021-06-28 22:27:21 +0800

    Added variable gridSize

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+let gridSize = getComputedStyle(document.documentElement).getPropertyValue('--gridSize');
 
-for(let i = 0 ; i < 16; i++){
+for(let i = 0 ; i < gridSize; i++){
     const div = document.createElement('div');
     div.classList.add(`rows`);
     div.id = `row${i+1}`;
@@ -8,11 +9,20 @@ for(let i = 0 ; i < 16; i++){
 
 const divs = document.querySelectorAll('div');
 
-for(let i = 0 ; i < 16; i++){
-    for(let j = 0 ; j < 16; j++){
+for(let i = 0 ; i < gridSize; i++){
+    for(let j = 0 ; j < gridSize; j++){
         const div = document.createElement('div');
         div.classList.add('grid');
         div.textContent = j+1;
         divs[i].appendChild(div);
     }
 }
+
+function setColorOnHover(item){
+    item.target.classList.add('grid-hover');
+}
+
+const grid = document.querySelectorAll('div[class="grid"]');
+
+grid.forEach(item => item.addEventListener('mouseover', setColorOnHover));
+
