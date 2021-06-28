017095355a5abf62533f96d4ceb189dc87d74304
Leong <stapler_1@hotmail.com>
2021-06-28 21:47:25 +0800

    Added border to grid

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const divs = document.querySelectorAll('div');
 for(let i = 0 ; i < 16; i++){
     for(let j = 0 ; j < 16; j++){
         const div = document.createElement('div');
+        div.classList.add('grid');
         div.textContent = j+1;
         divs[i].appendChild(div);
     }
